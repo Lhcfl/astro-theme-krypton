98bff092bb4e16350fa9046da9267ac6ec934856
Lhcfl <lhcfl@outlook.com>
2025-06-25 02:12:36 +0800

    🐛 fix: wordcloud should shrink

--- src/components/WordCloud.svelte
+++ src/components/WordCloud.svelte
@@ -14,9 +14,13 @@
   let canvasEl: HTMLElement;
 
   onMount(() => {
+    const divWidth = canvasEl.clientWidth;
+    const wfactor = Math.sqrt(Math.min(768, divWidth) / 768);
+    const factor = 14 * wfactor;
+    const gridSize = 18 * wfactor;
     WordCloud([canvasEl], {
-      gridSize: 18,
-      weightFactor: (size) => Math.log2(size + 1) * 14,
+      gridSize,
+      weightFactor: (size) => Math.log2(size + 1) * factor,
       click: ([name, number, href]) => {
         if (!href) return;
         const a = document.createElement("a");
